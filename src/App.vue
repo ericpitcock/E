1166abeaf44081723675b500e8bda3ee9cb36789
Eric Pitcock <ericpitcock@users.noreply.github.com>
2021-02-15 23:26:20 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <div
+      :class="['bounding-box', { 'bounding-box--selected': selected }]"
+      @click="selected = !selected"
+    >
+      <e :outline="true" v-if="selected" />
+      <e :outline="false" />
+    </div>
+  </div>
+</template>
+
+<script>
+import E from "@/components/E";
+
+export default {
+  name: "App",
+  data: () => ({
+    selected: false,
+  }),
+  components: {
+    E,
+  },
+};
+</script>
+
+<style lang="scss">
+$blue: #4f80ff;
+
+body {
+  margin: 0;
+  /* background: lightgray; */
+}
+#app {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.bounding-box {
+  position: relative;
+  border: 1px solid transparent;
+  &--selected {
+    border-color: $blue;
+  }
+}
+</style>
